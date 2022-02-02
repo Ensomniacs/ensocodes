ffc858c1f7c79eeaf296757e8e598d26bc2030fa
E <sxbrewer@ualr.edu>
2022-02-02 21:21:32 +0000

    changes to launch.jason, update circle script

--- circle/assets/js/script.js
+++ circle/assets/js/script.js
@@ -4,9 +4,8 @@ $( "#CircleForm" ).validate({
     function displayResults(){
         if ($( "#CircleForm" ).valid()) {
 
-            document.getElementById("diameter").innerHTML = "";
-            document.getElementById("circumfrence").innerHTML = "";
-            document.getElementById("area").innerHTML = "";
+            // read radius as a string
+            radius = document.getElementById("radius").value;
 
             // variables
             var radius;
@@ -16,24 +15,21 @@ $( "#CircleForm" ).validate({
             var circumfrencefp; 
             var area;
             var areafp;
-            var result;
 
-            // read radius as a string
-            radius = document.getElementById("radius").value;
-
-            //pars float calulations
+            // diameter
+            document.getElementById("diameter").innerHTML = "";
             diameterfp = parseFloat ( diameter );
-            circumfrencefp = parseFloat ( circumfrence);
-            areafp = parseFloat ( area );
-
-            // calculations
             diameter = calcDiameter(radius);
-            circumfrence = calcCircumfrence(radius);
-            area = calcArea(radius);
-
-            //display results as string
             document.getElementById("diameter").innerHTML = diameter.toString();
+
+            document.getElementById("circumfrence").innerHTML = "";
+            circumfrencefp = parseFloat ( circumfrence);
+            circumfrence = calcCircumfrence(radius);
             document.getElementById("circumfrence").innerHTML = circumfrence.toStrince();
+
+            document.getElementById("area").innerHTML = "";
+            areafp = parsFloat ( area );
+            area = calcArea(radius);
             document.getElementById("area").innerHTML = area.toString();
         }
     }
